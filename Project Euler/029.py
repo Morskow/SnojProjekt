ee27eda8d213aaeb7bb54ee5c590a3f856c6be8a
Morskow <43124663+Morskow@users.noreply.github.com>
2018-09-11 00:11:40 +0200

    Delete 029.py

--- Project Euler/029.py
+++
@@ -1,9 +0,0 @@
-seznam = []
-
-for a in range(2, 101):
-    for b in range(2, 101):
-        seznam.append(a ** b)
-
-# iz seznama naredimo mnozico, ki nam resi vse probleme
-
-print(len(set(seznam)))
