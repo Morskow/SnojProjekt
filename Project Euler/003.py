04e6d10bc4ac4a33974ea6baeb44afd0ebf28ce3
Morskow <43124663+Morskow@users.noreply.github.com>
2018-09-10 21:19:06 +0200

    Delete 003.py

--- Project Euler/003.py
+++
@@ -1,8 +0,0 @@
-def najvecji_prafaktor(stevilo):
-    for delitelj in range(2, stevilo):
-        if stevilo % delitelj == 0:
-            return najvecji_prafaktor(stevilo//delitelj)
-    return stevilo
-
-print(najvecji_prafaktor(600851475143))
-        
